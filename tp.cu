3a1c4468e0a0bb9495d2e3514c5269644a93db8f
Slace1 <theo.barberot@ensea.fr>
2022-12-05 14:46:03 +0100

    Multiplication des matrices

--- tp.cu
+++ tp.cu
@@ -6,7 +6,7 @@
 #include <cuda_runtime.h>
 
 #define N 4
-#define P 5
+#define P 4
 
 //Initialisation d'une matrice 
 void MatrixInit(float *M, int n, int p) {
@@ -32,72 +32,104 @@ void MatrixAdd(float *M1, float *M2, float *Mout, int n, int p) {
     }
 }
 
-/*
-_global_ void CudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p) {}
 
-void MatrixMult(float *M1, float *M2, float *Mout, int n) {}
+__global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p) {
+    for(int i = 0; i < n*p; i++){
+        Mout[i] = M1[i] + M2[i];
+    }
+}
 
-_global_ void CudaMatrixMult(float *M1, float *M2, float *Mout, int n) {}
-*/
+void MatrixMult(float *M1, float *M2, float *Mout, int n) {
+    for (int i=0 ; i<n ; i++){
+        for (int j=0 ; j<n ; j++){
+            float s = 0 ;
+            for (int k=0 ; k<n ; k++){
+                s = s + M1[i*n + k]*M2[k*n + j] ;
+            }
+            Mout[i*n + j] = s ;
+        }
+    }
+}
+
+__global__ void CudaMatrixMult(float *M1, float *M2, float *Mout, int n) {
+    for (int i=0 ; i<n ; i++){
+        for (int j=0 ; j<n ; j++){
+            float s = 0 ;
+            for (int k=0 ; k<n ; k++){
+                s = s + M1[i*n + k]*M2[k*n + j] ;
+            }
+            Mout[i*n + j] = s ;
+        }
+    }
+}
+
 
 int main() {
     //Initialistions des paramètres
-    float *M1, *M2, *M3;
+    float *M1, *M2, *M3, *M4, *M5, *M6;
+    float *d_M1, *d_M2, *d_M4, *d_M6;
     
     
-    //Allocution de mémoire pour une matrice 
+    //Allocution de mémoire pour une matrice sur le CPU
     M1 = (float*)malloc(sizeof(float) * N * P) ;
     M2 = (float*)malloc(sizeof(float) * N * P) ; 
     M3 = (float*)malloc(sizeof(float) * N * P) ; 
+    M4 = (float*)malloc(sizeof(float) * N * P) ;
+    M5 = (float*)malloc(sizeof(float) * N * P) ;
+    M6 = (float*)malloc(sizeof(float) * N * P) ;
+
+    //Allocution de mémoire pour une matrice sur le GPU
+    cudaMalloc((void**)&d_M1, sizeof(float) * N * P);
+    cudaMalloc((void**)&d_M2, sizeof(float) * N * P);
+    cudaMalloc((void**)&d_M4, sizeof(float) * N * P);
+    cudaMalloc((void**)&d_M6, sizeof(float) * N * P);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 
     //Process
+    //Initialisation des matrices
     MatrixInit(M1, N, P) ;
     MatrixInit(M2, N, P) ;
-    MatrixAdd(M1, M2, M3, N, P) ;
 
+    //Copie des données du CPU vers le GPU (device)
+    cudaMemcpy(d_M1, M1, sizeof(float) * N * P, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_M2, M2, sizeof(float) * N * P, cudaMemcpyHostToDevice);
+
+    MatrixAdd(M1, M2, M3, N, P) ; //Addition de matrices sur le CPU
+    MatrixMult(M1, M2, M5, N); //Multiplications de matrices sur le CPU
+
+    cudaMatrixAdd<<<1,1>>>(d_M1, d_M2, d_M4, N, P) ; //Addition de matrics sur le GPU
+    CudaMatrixMult<<<1,1>>>(d_M1, d_M2, d_M6, N);
+
+    //Copie des données du GPU vers le CPU (local) 
+    cudaMemcpy(M4, d_M4, sizeof(float) * N * P, cudaMemcpyDeviceToHost) ;
+    cudaMemcpy(M6, d_M6, sizeof(float) * N * P, cudaMemcpyDeviceToHost) ;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+    //Affichage
     MatrixPrint(M1, N, P) ;
     MatrixPrint(M2, N, P) ;
     MatrixPrint(M3, N, P) ;
+    MatrixPrint(M4, N, P) ;
+    MatrixPrint(M5, N, P) ;
+    MatrixPrint(M6, N, P) ;
 
 
-    //Libération de la mémoire
+    //Libération de la mémoire du CPU
 
     free(M1);
     free(M2);
     free(M3);
-/*   
-    float *a, *b, *out;
-    float *d_a, *d_b, *d_out;
+    free(M4);
+    free(M5);
+    free(M6);
 
-    // Allocate memory
-    a   = (float*)malloc(sizeof(float) * N);
-    b   = (float*)malloc(sizeof(float) * N);
-    out = (float*)malloc(sizeof(float) * N);
+    //Libération de la mémoire du GPU
+    cudaFree(d_M1);
+    cudaFree(d_M2);
+    cudaFree(d_M4);
+    cudaFree(d_M6);
 
-    cudaMalloc((void**)&d_a, sizeof(float)*N);
-    cudaMalloc((void**)&d_b, sizeof(float)*N);
-    cudaMalloc((void**)&d_out, sizeof(float)*N);
-
-    MatrixInit(*d_out, 10, 4) ;
-    MatrixPrint(*d_out, 10, 4) ;
-
-    cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
-
-    // Main function
-    vector_add<<<1,1>>>(d_out, d_a, d_b, N);
-
-    cudaMemcpy(out, d_out, sizeof(float)*N, cudaMemcpyDeviceToHost);
-
-
-
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_out);
-
-    free(a);
-    free(b);
-    free(out);
-
-*/
 }
